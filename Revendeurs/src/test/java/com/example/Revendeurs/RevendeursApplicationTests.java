f9f9d0d6852405ebd6fcb3039634b4415a50acfc
RobinMax23 <58518275+RobinMax23@users.noreply.github.com>
2023-09-08 12:08:09 +0200

    Update RevendeursApplicationTests.java

--- Revendeurs/src/test/java/com/example/Revendeurs/RevendeursApplicationTests.java
+++ Revendeurs/src/test/java/com/example/Revendeurs/RevendeursApplicationTests.java
@@ -1,1 +1,14 @@
+package com.example.Revendeurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevendeursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
 
